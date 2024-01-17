c26bfe26bbde19587266eadd69180b45a4d3769a
ESZI\vargab <balazsvarga451@gmail.com>
2024-01-17 09:20:10 +0100

    GameWindow létrehozása

---
+++ dominogameszko/dominogameszko/GameWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace dominogameszko
+{
+	/// <summary>
+	/// Interaction logic for GameWindow.xaml
+	/// </summary>
+	public partial class GameWindow : Window
+	{
+		public GameWindow()
+		{
+			InitializeComponent();
+			for (int i = 0; i < 50; i++)
+			{
+				Table.RowDefinitions.Add(new RowDefinition());
+				Table.ColumnDefinitions.Add(new ColumnDefinition());
+			}
+		}
+
+	}
+}
